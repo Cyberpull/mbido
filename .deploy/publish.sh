73f2909ebbc625d069e8a36111da0525a2e54933
Christian Ezeani <christian.ezeani@gmail.com>
2024-11-25 23:49:09 +0100

    Fix identity

--- .deploy/publish.sh
+++ .deploy/publish.sh
@@ -4,6 +4,10 @@ set -e
 # Publish mbido
 # ===========================
 
+git config --global user.name "$ID_NAME"
+
+git config --global user.email "$ID_EMAIL"
+
 npm set //registry.npmjs.org/:_authToken $TOKEN
 
 npm version $LIB_VERSION
